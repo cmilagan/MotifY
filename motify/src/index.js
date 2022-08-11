a7e07fe1f73a5679233102e7615ba0a56cb84d69
Christian Ilagan <cmilagan7@gmail.com>
2022-08-11 18:23:03 +1000

    Adding insertion point to allow notificaiton to pop up on current web page

--- motify/src/index.js
+++ motify/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import Notification from './Notification';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const insertionPoint = document.createElement("div");
+insertionPoint.id = "insertion-point";
+document.body.parentNode.insertBefore(insertionPoint, document.body);
+
+ReactDOM.render(
   <React.StrictMode>
     <Notification />
-  </React.StrictMode>
+  </React.StrictMode>,
+  insertionPoint
 );
 
 // If you want to start measuring performance in your app, pass a function
